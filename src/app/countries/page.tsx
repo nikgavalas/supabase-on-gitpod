68aa7f65ecc13623cafa5ea9c50b828a2370509d
nikgavalas <nikgavalas@gmail.com>
2023-05-01 13:08:29 +0000

    Prisma seeding working

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,48 +1,21 @@
-import { supabase } from '@/src/lib/supabaseClient';
-
-type Country = {
-  id: string;
-  name: string;
-}
+import { Country } from '@prisma/client';
+import { prisma } from '@/src/lib/prisma-client';
 
 export default async function Page() {
   const countries = await getData();
   return (
     <div>
-    <h1>Countries</h1>
-    <ul>
-      {countries.map((country) => (
-        <li key={country.id}>{country.name}</li>
-      ))}
-    </ul>
+      <h1>Countries</h1>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.id}>{country.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 async function getData(): Promise<Country[]> {
-  // const res = await fetch('https://api.example.com/...');
-  let { data } = await supabase.from('countries').select()
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  return (data || []) as Country[];
-  // Recommendation: handle errors
-  // if (!res.ok) {
-  //   // This will activate the closest `error.js` Error Boundary
-  //   throw new Error('Failed to fetch data');
-  // }
-
-  // return res.json();
+  const data = await prisma.country.findMany();
+  return data;
 }
-
-// export async function getServerSideProps() {
-//   let { data } = await supabase.from('countries').select()
-
-//   return {
-//     props: {
-//      countries: data
-//     },
-//   }
-// }
-
-// export default Page;
